976a332b15cbf5c8c26690a679fa9102689f459b
lucas-diet <lucas.d1@gmx.net>
2024-04-22 18:12:03 +0200

    init gui

---
+++ gui.py
@@ -0,0 +1,20 @@
+
+from tkinter import *
+
+class App():
+    def __init__(self, master):
+        self.master = master
+        master.title('BloodPressureSim')
+        master.geometry('800x900')
+
+   
+
+
+
+
+if __name__ == '__main__':
+    root = Tk()
+    app = App(root)
+    root.mainloop()
+    
+
