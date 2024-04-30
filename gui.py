256dac2cbebca748473b4ad1b6992cf51cdb1001
lucas-diet <lucas.d1@gmx.net>
2024-04-30 16:21:52 +0200

    Gui angepasst & zeile gelöscht

--- gui.py
+++ gui.py
@@ -5,46 +5,39 @@ class GuiApp():
     def __init__(self, master):
         self.master = master
         master.title('Blood Pressure Simulator')
-        master.geometry('400x200')
+        master.geometry('350x600')
 
         self.duration_holder = 'duration'
         self.heartRate_holder = 'heart rate'
 
-        self.placeholders = {
-            'duration': 'duration',
-            'heartRate': 'heart rate',
-            'systolic': 'systolic'}
+        #self.placeholders = {
+        #    'duration': 'duration',
+        #    'heartRate': 'heart rate',
+        #    'systolic': 'systolic'}
+        
+        self.duration_label = Label(master, text='duration:', width=12)
+        self.duration_label.place(x=5,y=10)
 
-        # Eingabefelder erstellen und platzieren
-        self.entries = {}
-        row = 0
-        column = 0
-        for key, placeholder in self.placeholders.items():
-            entry = Entry(self.master, width=10)
-            entry.insert(0, placeholder)
-            entry.grid(row=row, column=column, padx=10, pady=10)
-            entry.bind('<FocusIn>', lambda event, widget=entry, key=key: self.onFocusIn(widget, key))
-            entry.bind('<FocusOut>', lambda event, widget=entry, key=key, placeholder=placeholder: self.onFocusOut(widget, key, placeholder))
-            self.entries[key] = entry
-            entry.config(bg='white', fg='black')
-            row += 1
-            if row == 3:
-                row = 0
-                column += 1
-            elif column == 2:
-                #TODO: Zukunft, je nachdem wie viele Paramter
-                pass
+        self.duration_input = Entry(master, width=10, bg='white', fg='black')
+        self.duration_input.place(x=150,y=10)
 
+        self.heartRate_lable = Label(master, text='heart rate:', width=12)
+        self.heartRate_lable.place(x=5,y=50)
 
-    def onFocusIn(self, widget, key):
-        #Wird aufgerufen, wenn das Entry-Feld den Fokus erhält.
-        if widget.get() == self.placeholders[key]:
-            widget.delete(0, END)
+        self.heartRate_input = Entry(master, width=10, bg='white', fg='black')
+        self.heartRate_input.place(x=150, y=50)
 
-    def onFocusOut(self, widget, key, placeholder):
-        #Wird aufgerufen, wenn das Entry-Feld den Fokus verliert.
-        if not widget.get():
-            widget.insert(0, placeholder)
+        self.systolic_label = Label(master, text='systolic pressure:', width=12)
+        self.systolic_label.place(x=5, y=90)
+
+        self.systolic_input = Entry(master, width=10, bg='white', fg='black')
+        self.systolic_input.place(x=150,y=90)
+
+        self.diastolic_label = Label(master, text='diastolic pressure:', width=12)
+        self.diastolic_label.place(x=5, y=130)
+
+        self.diastolic_input = Entry(master, width=10, bg='white', fg='black')
+        self.diastolic_input.place(x=150,y=130)
 
 if __name__ == '__main__':
     root = Tk()
