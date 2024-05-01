5324bd0e4d5f626abb99096c492ef9057bf324db
lucas-diet <lucas.d1@gmx.net>
2024-05-01 12:30:55 +0200

    Funktion um zu plotten

---  vascularSystem.py
+++  vascularSystem.py
@@ -4,14 +4,19 @@ import matplotlib.pyplot as plt
 
 class VascularSystem():
 
-    def __init__(self):
-        pass
+    def __init__(self, aor_rad, art1_rad, art2_rad,cap_rad, ven1_rad, ven2_rad, vc_rad):
+        self.aor_rad = aor_rad
+        self.art1_rad = art1_rad
+        self.art2_rad = art2_rad
+        self.cap_rad = cap_rad
+        self.ven1_rad = ven1_rad
+        self.ven2_rad = ven2_rad
+        self.vc_rad = vc_rad
 
-    def aorta(self, radius, wallThickness=2000, number=1):
+    def aorta(self, wallThickness=2000, number=1):
         """_summary_
             
         Args:
-            radius (_type_): _description_
             wallThickness (int, optional): _description_. Defaults to 2000.
             number (int, optional): _description_. Defaults to 1.
         Returns:
@@ -22,6 +27,7 @@ class VascularSystem():
             radius
             wallThickness 
         """
+        radius = self.aor_rad
         theta = np.linspace(0, 2*np.pi, 150)
 
         # wallThickness und radius ist in µm
@@ -38,17 +44,26 @@ class VascularSystem():
         x1 = cs * np.cos(theta)
         y1 = cs * np.sin(theta)
 
+        # Durchmesser halbieren
+        x /= 2
+        y /= 2
+
+        x1 /= 2
+        y1 /= 2
+
         return x, y, x1, y1, radius, wallThickness
     
-    def arteries(self, radius, wallThickness=1000):
+    def arteries(self, wallThickness=1000):
         """_summary_
 
         Args:
-            radius (_type_): _description_
             wallThickness (int, optional): _description_. Defaults to 1000.
         Returns:
 
         """
+        #number = self.aorta()^2
+        radius = self.art1_rad
+
         theta = np.linspace(0, 2*np.pi, 150)
 
         # wallThickness und radius ist in µm
@@ -65,13 +80,19 @@ class VascularSystem():
         x1 = cs * np.cos(theta)
         y1 = cs * np.sin(theta)
 
+        # Durchmesser halbieren
+        x /= 2
+        y /= 2
+
+        x1 /= 2
+        y1 /= 2
+
         return x, y, x1, y1, radius, wallThickness
 
-    def arterioles(self, radius, wallThickness=30):
+    def arterioles(self, wallThickness=30):
         """_summary_
             
         Args:
-            radius (_type_): _description_
             wallThickness (int, optional): _description_. Defaults to 2000.
             number (int, optional): _description_. Defaults to 1.
         Returns:
@@ -82,6 +103,7 @@ class VascularSystem():
             radius
             wallThickness 
         """
+        radius = self.art2_rad
         theta = np.linspace(0, 2*np.pi, 150)
 
         # wallThickness und radius ist in µm
@@ -98,13 +120,19 @@ class VascularSystem():
         x1 = cs * np.cos(theta)
         y1 = cs * np.sin(theta)
 
+        # Durchmesser halbieren
+        x /= 2
+        y /= 2
+
+        x1 /= 2
+        y1 /= 2
+
         return x, y, x1, y1, radius, wallThickness
 
-    def capillaries(self, radius, wallThickness=1):
+    def capillaries(self, wallThickness=1):
         """_summary_
             
         Args:
-            radius (_type_): _description_
             wallThickness (int, optional): _description_. Defaults to 2000.
             number (int, optional): _description_. Defaults to 1.
         Returns:
@@ -115,6 +143,7 @@ class VascularSystem():
             radius
             wallThickness 
         """
+        radius = self.cap_rad
         theta = np.linspace(0, 2*np.pi, 150)
 
         # wallThickness und radius ist in µm
@@ -131,13 +160,19 @@ class VascularSystem():
         x1 = cs * np.cos(theta)
         y1 = cs * np.sin(theta)
 
+        # Durchmesser halbieren
+        x /= 2
+        y /= 2
+
+        x1 /= 2
+        y1 /= 2
+
         return x, y, x1, y1, radius, wallThickness
 
-    def venules(self, radius, wallThickness=2):
+    def venules(self, wallThickness=2):
         """_summary_
             
         Args:
-            radius (_type_): _description_
             wallThickness (int, optional): _description_. Defaults to 2000.
             number (int, optional): _description_. Defaults to 1.
         Returns:
@@ -148,6 +183,7 @@ class VascularSystem():
             radius
             wallThickness 
         """
+        radius = self.ven1_rad
         theta = np.linspace(0, 2*np.pi, 150)
 
         # wallThickness und radius ist in µm
@@ -164,13 +200,19 @@ class VascularSystem():
         x1 = cs * np.cos(theta)
         y1 = cs * np.sin(theta)
 
+        # Durchmesser halbieren
+        x /= 2
+        y /= 2
+
+        x1 /= 2
+        y1 /= 2
+
         return x, y, x1, y1, radius, wallThickness
 
-    def veins(self, radius, wallThickness=500):
+    def veins(self, wallThickness=500):
         """_summary_
             
         Args:
-            radius (_type_): _description_
             wallThickness (int, optional): _description_. Defaults to 2000.
             number (int, optional): _description_. Defaults to 1.
         Returns:
@@ -181,6 +223,7 @@ class VascularSystem():
             radius
             wallThickness 
         """
+        radius = self.ven2_rad
         theta = np.linspace(0, 2*np.pi, 150)
 
         # wallThickness und radius ist in µm
@@ -197,13 +240,19 @@ class VascularSystem():
         x1 = cs * np.cos(theta)
         y1 = cs * np.sin(theta)
 
+        # Durchmesser halbieren
+        x /= 2
+        y /= 2
+
+        x1 /= 2
+        y1 /= 2
+
         return x, y, x1, y1, radius, wallThickness
 
-    def venaCava(self, radius, wallThickness=1500, number=1):
+    def venaCava(self, wallThickness=1500, number=1):
         """_summary_
             
         Args:
-            radius (_type_): _description_
             wallThickness (int, optional): _description_. Defaults to 2000.
             number (int, optional): _description_. Defaults to 1.
         Returns:
@@ -214,6 +263,7 @@ class VascularSystem():
             radius
             wallThickness 
         """
+        radius = self.vc_rad
         theta = np.linspace(0, 2*np.pi, 150)
 
         # wallThickness und radius ist in µm
@@ -229,82 +279,91 @@ class VascularSystem():
         cs = radius + wallThickness
         x1 = cs * np.cos(theta)
         y1 = cs * np.sin(theta)
+
+        # Durchmesser halbieren
+        x /= 2
+        y /= 2
+
+        x1 /= 2
+        y1 /= 2
 
         return x, y, x1, y1, radius, wallThickness
     
-    def normVessels(self):
-        pass
+    def plotter(self, lims=[]):
+        fig, axs = plt.subplots(2,4)
+        fig.set_figheight(6)
+        fig.set_figwidth(12)
+        
+        for i in range(0, 4):
+            for j in range(0, 2):
+                if i == 0 and j == 0:
+                    axs[j,i].plot(aorta[0],aorta[1])
+                    axs[j,i].plot(aorta[2],aorta[3])
+                    axs[j,i].title.set_text('Aorta')
+                
+                elif i == 1 and j == 0:
+                    axs[j,i].plot(arteries[0],arteries[1])
+                    axs[j,i].plot(arteries[2],arteries[3])
+                    axs[j,i].title.set_text('Arterien')
+
+                elif i == 2 and j == 0:
+                    axs[j,i].plot(arterioles[0],arterioles[1])
+                    axs[j,i].plot(arterioles[2],arterioles[3])
+                    axs[j,i].title.set_text('Arteriole')
+                
+                elif i == 3 and j == 0:
+                    axs[j,i].plot(capillaries[0],capillaries[1])
+                    axs[j,i].plot(capillaries[2],capillaries[3])
+                    axs[j,i].title.set_text('Kappilare')
+                
+                elif i == 0 and j == 1:
+                    axs[j,i].plot(venules[0],venules[1])
+                    axs[j,i].plot(venules[2],venules[3])
+                    axs[j,i].title.set_text('Venole')
+
+                elif i == 1 and j == 1:
+                    axs[j,i].plot(veins[0],veins[1])
+                    axs[j,i].plot(veins[2],veins[3])
+                    axs[j,i].title.set_text('Vene')
+                
+                elif i == 2 and j == 1:
+                    axs[j,i].plot(venaCava[0],venaCava[1])
+                    axs[j,i].plot(venaCava[2],venaCava[3])
+                    axs[j,i].title.set_text('V. cava')
+                
+                if len(lims) == 2:
+                    if lims[0] > 0:
+                        lims[0] *= -1
+                    
+                    axs[j,i].set_xlim(lims[0], lims[1])
+                    axs[j,i].set_ylim(lims[0], lims[1])
+
+                axs[j,i].grid(True)
+                axs[j,i].tick_params(labelleft = False)
+
+                    
 
 
+"""
+Parameter (Radien): 
+    1. aorta
+    2. arteries
+    3. arterioles
+    4. capillaries
+    5. venules
+    6. veins
+    7. venaCava 
+""" 
+vs = VascularSystem(20000, 4000, 20, 8, 20, 5000, 30000)
+aorta = vs.aorta()
+arteries = vs.arteries()
+arterioles = vs.arterioles()
+capillaries = vs.capillaries()
+venules = vs.venules()
+veins = vs.veins()
+venaCava = vs.venaCava()
 
-vs = VascularSystem()
-aorta = vs.aorta(20000)
-arteries = vs.arteries(4000)
-arterioles = vs.arterioles(20)
-capillaries = vs.capillaries(8)
-venules = vs.venules(20)
-veins = vs.veins(5000)
-venaCava = vs.venaCava(30000)
-
-fig, axs = plt.subplots(2,4)
-fig.set_figheight(6)
-fig.set_figwidth(12)
-
-axs[0,0].plot(aorta[0],aorta[1])
-axs[0,0].plot(aorta[2],aorta[3])
-axs[0,0].grid(True)
-axs[0,0].tick_params(labelleft = False)
-axs[0,0].set_xlim(-32,32)
-axs[0,0].set_ylim(-32,32)
-axs[0,0].title.set_text('Aorta')
-
-axs[0,1].plot(arteries[0], arteries[1])
-axs[0,1].plot(arteries[2], arteries[3])
-axs[0,1].grid(True)
-axs[0,1].tick_params(labelleft = False)
-axs[0,1].set_xlim(-32,32)
-axs[0,1].set_ylim(-32,32)
-axs[0,1].title.set_text('Arterie')
-
-axs[0,2].plot(arterioles[0], arterioles[1])
-axs[0,2].plot(arterioles[2], arterioles[3])
-axs[0,2].grid(True)
-axs[0,2].tick_params(labelleft = False)
-axs[0,2].set_xlim(-32,32)
-axs[0,2].set_ylim(-32,32)
-axs[0,2].title.set_text('Arteriole')
-
-axs[0,3].plot(capillaries[0], capillaries[1])
-axs[0,3].plot(capillaries[2], capillaries[3])
-axs[0,3].grid(True)
-axs[0,3].tick_params(labelleft = False)
-axs[0,3].set_xlim(-32,32)
-axs[0,3].set_ylim(-32,32)
-axs[0,3].title.set_text('Kapillare')
-
-axs[1,0].plot(venules[0], venules[1])
-axs[1,0].plot(venules[2], venules[3])
-axs[1,0].grid(True)
-axs[1,0].tick_params(labelleft = False)
-axs[1,0].set_xlim(-32,32)
-axs[1,0].set_ylim(-32,32)
-axs[1,0].title.set_text('Venole')
-
-axs[1,1].plot(veins[0], veins[1])
-axs[1,1].plot(veins[2], veins[3])
-axs[1,1].grid(True)
-axs[1,1].tick_params(labelleft = False)
-axs[1,1].set_xlim(-32,32)
-axs[1,1].set_ylim(-32,32)
-axs[1,1].title.set_text('Vene')
-
-axs[1,2].plot(venaCava[0], venaCava[1])
-axs[1,2].plot(venaCava[2], venaCava[3])
-axs[1,2].grid(True)
-axs[1,2].tick_params(labelleft = False)
-axs[1,2].set_xlim(-32,32)
-axs[1,2].set_ylim(-32,32)
-axs[1,2].title.set_text('V. cava')
-
+lims = [-20, 20]
+vs.plotter(lims)
 
 plt.show()
