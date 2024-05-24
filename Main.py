a944928ea2b882f068fafd786ead5d2a4efcf9d4
lucas-diet <lucas.d1@gmx.net>
2024-05-24 14:30:48 +0200

    Anpassungen

--- Main.py
+++ Main.py
@@ -5,7 +5,9 @@ from heart import *
 from bodySystem import *
 from sensor import *
 
+####################
 #### Parameter #####
+####################
 
 radi = [20000, 4000, 20, 8, 20, 5000, 30000]
 viscocity = 1
@@ -18,14 +20,13 @@ pres0 = 70
 maxTime = 10
 dt = 0.01
 
-lims = [-17, 17]
-lumRadiF = [1, 1, 1, 1, 1, 1, 1] # array, um den inneren Radius anpassen zu können -> ein Faktor zu skalieren
-
 nums = [1, 2, 4, 16, 4, 2, 1]
 lens = [200, 150, 100, 50, 100, 150, 300] # in mm
 type = ['aorta', 'arteries', 'arterioles', 'capillaries', 'venules', 'veins', 'venaCava']
 
-##### BloodPressure #####
+##########################
+##### Blood Pressure ##### ---> Wie solle es ca. am Ende aussehen. Dient nur als Einstieg.
+##########################
 
 duration = 60       # Sekunden
 heart_rate = 10     # Schläge pro Minute
@@ -33,24 +34,34 @@ systolic = 120      # TODO: soll noch simuliert werden mit Parametern
 diastolic = 80      # TODO: soll noch simuliert werden mit Parametern
 
 bpSim = BloodPressure(duration, heart_rate, systolic, diastolic)
-#bp = bpSim.simulateBP()
+bp = bpSim.simulateBP()
 
 # Plot der simulierten Blutdruckwerte
-#bpSim.bpPlotter()
+bpSim.bpPlotter()
 
+################
 #### Heart #####
+################
 
 h = Heart(radi, viscocity, heartRate, strokeVolume, edv, esv, pres0, maxTime, dt)
-#h.hpPlotter()
+h.hpPlotter()
 
+#######################
 ##### Body System #####
+#######################
+
+lims = [-17, 17]
+lumRadiF = [1, 1, 1, 1, 1, 1, 1] # array, um den inneren Radius anpassen zu können -> ein Faktor zu skalieren
 
 bs = BodySystem(radi, viscocity, heartRate, strokeVolume, edv, esv, pres0, maxTime)
-#bs.vesselPlotter(lumRadiF, lims)
-#bs.resisPrinter(type, lens, radi, lumRadiF, nums)
-#bs.vpPlotter()
+bs.vesselPlotter(lumRadiF, lims)
+bs.resisPrinter(type, lens, radi, lumRadiF, nums)
+bs.vpPlotter()
 
+##################
 ##### Sensor #####
+##################
+
 data = [bs.aortaPressure, bs.arteriePressure, bs.arteriolPressure, bs.capillarePressure, bs.venolePressure, bs.venePressure, bs.vCavaPressure]
 dataC = [h.bloodPressure_RV, h.bloodPressure_LV,  bs.aortaPressure, bs.arteriePressure, bs.arteriolPressure, bs.capillarePressure, bs.venolePressure, bs.venePressure, bs.vCavaPressure]
 types = ['Rechter Ventrikel', 'Linker Ventrikel', 'Aorta', 'Arterie', 'Arteriole', 'Kapillare', 'Venole', 'Vene', 'V. Cava']
@@ -60,5 +71,5 @@ s = Sensor(radi, viscocity, heartRate, strokeVolume, edv, esv, pres0, maxTime, d
 h.heartSimulation()
 bs.vesselSimulator()
 
-#s.presPlotter(data)
+s.presPlotter(data)
 s.presPrinter(dataC, types)
